77b24ed4ef7eb97bed9c38f7a329954ad088724c
Gormogon <Gormogon@users.noreply.github.com>
2016-05-29 15:29:21 -0400

    Make build config consistent, move to .NET 4.6.1 to fully take advantage of C# 6, update Fody, and update Assembly Info.

--- StardewModdingAPI/Properties/AssemblyInfo.cs
+++ StardewModdingAPI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("StardewModdingAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Stardew Modding API (SMAPI)")]
+[assembly: AssemblyDescription("Stardew Valley modding API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StardewModdingAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Stardew Modding API (SMAPI)")]
+[assembly: AssemblyCopyright("Copyright © SMAPI Dev Team 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.40.0.0")]
+[assembly: AssemblyFileVersion("0.40.0.0")]
