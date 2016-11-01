310f705f1912d0bcbf406a8d7d76fdf1347d0459
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-11-01 18:07:09 -0400

    format & document  version class

--- src/StardewModdingAPI/Version.cs
+++ src/StardewModdingAPI/Version.cs
@@ -2,22 +2,44 @@
 
 namespace StardewModdingAPI
 {
+    /// <summary>A semantic mod version with an optional build tag.</summary>
     public struct Version
     {
+        /*********
+        ** Accessors
+        *********/
+        /// <summary>The major version incremented for major API changes.</summary>
         public int MajorVersion { get; set; }
+
+        /// <summary>The minor version incremented for backwards-compatible changes.</summary>
         public int MinorVersion { get; set; }
+
+        /// <summary>The patch version for backwards-compatible bug fixes.</summary>
         public int PatchVersion { get; set; }
+
+        /// <summary>An optional build tag.</summary>
         public string Build { get; set; }
 
+        /// <summary>A string representation of the version.</summary>
         [JsonIgnore]
         public string VersionString => $"{MajorVersion}.{MinorVersion}.{PatchVersion} {Build}";
 
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>Construct an instance.</summary>
+        /// <param name="major">The major version incremented for major API changes.</param>
+        /// <param name="minor">The minor version incremented for backwards-compatible changes.</param>
+        /// <param name="patch">The patch version for backwards-compatible bug fixes.</param>
+        /// <param name="build">An optional build tag.</param>
         public Version(int major, int minor, int patch, string build)
         {
-            MajorVersion = major;
-            MinorVersion = minor;
-            PatchVersion = patch;
-            Build = build;
+            this.MajorVersion = major;
+            this.MinorVersion = minor;
+            this.PatchVersion = patch;
+            this.Build = build;
+        }
         }
     }
 }
