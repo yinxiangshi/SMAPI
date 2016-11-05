b7b69707eb83fac01b30ad5b94bb8a2672fc2180
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-11-04 20:13:58 -0400

    document inventory changed event objects

--- src/StardewModdingAPI/Inheritance/ItemStackChange.cs
+++ src/StardewModdingAPI/Inheritance/ItemStackChange.cs
@@ -2,10 +2,19 @@
 
 namespace StardewModdingAPI.Inheritance
 {
+    /// <summary>Represents an inventory slot that changed.</summary>
     public class ItemStackChange
     {
+        /*********
+        ** Accessors
+        *********/
+        /// <summary>The item in the slot.</summary>
         public Item Item { get; set; }
+
+        /// <summary>The amount by which the item's stack size changed.</summary>
         public int StackChange { get; set; }
+
+        /// <summary>How the inventory slot changed.</summary>
         public ChangeType ChangeType { get; set; }
     }
 }
