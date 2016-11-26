4df199985558caee3275fdd65762bdc3e1d040c6
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-11-26 16:10:41 -0500

    move cache struct into its own file (#166)

--- src/StardewModdingAPI/Framework/ModAssemblyLoader.cs
+++ src/StardewModdingAPI/Framework/ModAssemblyLoader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography;
 using Mono.Cecil;
+using StardewModdingAPI.Framework.AssemblyRewriting;
 
 namespace StardewModdingAPI.Framework
 {
@@ -90,40 +91,6 @@ namespace StardewModdingAPI.Framework
             string cacheAssemblyPath = Path.Combine(dirPath, $"{key}.dll");
             string cacheHashPath = Path.Combine(dirPath, $"{key}.hash");
             return new CachePaths(dirPath, cacheAssemblyPath, cacheHashPath);
-        }
-
-        /*********
-        ** Private objects
-        *********/
-        /// <summary>Contains the paths for an assembly's cached data.</summary>
-        private struct CachePaths
-        {
-            /*********
-            ** Accessors
-            *********/
-            /// <summary>The directory path which contains the assembly.</summary>
-            public string Directory { get; }
-
-            /// <summary>The file path of the assembly file.</summary>
-            public string Assembly { get; }
-
-            /// <summary>The file path containing the MD5 hash for the assembly.</summary>
-            public string Hash { get; }
-
-
-            /*********
-            ** Public methods
-            *********/
-            /// <summary>Construct an instance.</summary>
-            /// <param name="directory">The directory path which contains the assembly.</param>
-            /// <param name="assembly">The file path of the assembly file.</param>
-            /// <param name="hash">The file path containing the MD5 hash for the assembly.</param>
-            public CachePaths(string directory, string assembly, string hash)
-            {
-                this.Directory = directory;
-                this.Assembly = assembly;
-                this.Hash = hash;
-            }
         }
     }
 }
