043508ed4264a3024b281665bfbf6e073c029fdf
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-03-03 20:22:30 -0500

    add texture patching to content events (#173)

--- src/StardewModdingAPI/IContentEventHelper.cs
+++ src/StardewModdingAPI/IContentEventHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace StardewModdingAPI
 {
@@ -45,6 +47,16 @@ namespace StardewModdingAPI
         /// <param name="value">A callback which accepts the current value and returns the new value.</param>
         /// <exception cref="InvalidOperationException">The content being read isn't a dictionary.</exception>
         void SetDictionaryEntry<TKey, TValue>(TKey key, Func<TValue, TValue> value);
+
+        /// <summary>Overwrite part of the image.</summary>
+        /// <param name="source">The image to patch into the content.</param>
+        /// <param name="sourceArea">The part of the <paramref name="source"/> to copy (or <c>null</c> to take the whole texture). This must be within the bounds of the <paramref name="source"/> texture.</param>
+        /// <param name="targetArea">The part of the content to patch (or <c>null</c> to patch the whole texture). The original content within this area will be erased. This must be within the bounds of the existing spritesheet.</param>
+        /// <param name="patchMode">Indicates how an image should be patched.</param>
+        /// <exception cref="ArgumentNullException">One of the arguments is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The <paramref name="targetArea"/> is outside the bounds of the spritesheet.</exception>
+        /// <exception cref="InvalidOperationException">The content being read isn't an image.</exception>
+        void PatchImage(Texture2D source, Rectangle? sourceArea = null, Rectangle? targetArea = null, PatchMode patchMode = PatchMode.Replace);
 
         /// <summary>Replace the entire content value with the given value. This is generally not recommended, since it may break compatibility with other mods or different versions of the game.</summary>
         /// <param name="value">The new content value.</param>
