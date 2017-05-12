dc4ad15afee05cd3c474273f921314cb0656d76c
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-05-11 22:32:47 -0400

    fix IManifest fields being settable

--- src/StardewModdingAPI/IManifest.cs
+++ src/StardewModdingAPI/IManifest.cs
@@ -5,28 +5,31 @@ namespace StardewModdingAPI
     /// <summary>A manifest which describes a mod for SMAPI.</summary>
     public interface IManifest
     {
+        /*********
+        ** Accessors
+        *********/
         /// <summary>The mod name.</summary>
-        string Name { get; set; }
+        string Name { get; }
 
         /// <summary>A brief description of the mod.</summary>
-        string Description { get; set; }
+        string Description { get; }
 
         /// <summary>The mod author's name.</summary>
         string Author { get; }
 
         /// <summary>The mod version.</summary>
-        ISemanticVersion Version { get; set; }
+        ISemanticVersion Version { get; }
 
         /// <summary>The minimum SMAPI version required by this mod, if any.</summary>
-        string MinimumApiVersion { get; set; }
+        string MinimumApiVersion { get; }
 
         /// <summary>The unique mod ID.</summary>
-        string UniqueID { get; set; }
+        string UniqueID { get; }
 
         /// <summary>The name of the DLL in the directory that has the <see cref="Mod.Entry"/> method.</summary>
-        string EntryDll { get; set; }
+        string EntryDll { get; }
 
         /// <summary>Any manifest fields which didn't match a valid field.</summary>
-        IDictionary<string, object> ExtraFields { get; set; }
+        IDictionary<string, object> ExtraFields { get; }
     }
 }
