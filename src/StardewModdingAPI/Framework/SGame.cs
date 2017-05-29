9d9383e235dd732a5b90145dcf404a2daa5b7532
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-05-28 20:58:31 -0400

    drop special case for SDV 1.2.29 bug (min version is now 1.2.30)

--- src/StardewModdingAPI/Framework/SGame.cs
+++ src/StardewModdingAPI/Framework/SGame.cs
@@ -584,13 +584,6 @@ namespace StardewModdingAPI.Framework
                     return;
                 }
 
-                // abort in known unrecoverable cases
-                if (Game1.toolSpriteSheet?.IsDisposed == true)
-                {
-                    this.Monitor.ExitGameImmediately("the game unexpectedly disposed the tool spritesheet, so it crashed trying to draw a tool. This is a known bug in Stardew Valley 1.2.29, and there's no way to recover from it.");
-                    return;
-                }
-
                 // recover sprite batch
                 try
                 {
