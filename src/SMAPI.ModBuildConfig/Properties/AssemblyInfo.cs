af68910685326a660cc88cd92582b38cbc0d9b2f
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-10-08 00:20:13 -0400

    convert mod build config into .NET project to simplify C# build tasks

---
+++ src/SMAPI.ModBuildConfig/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StardewModdingAPI.ModBuildConfig")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("ea4f1e80-743f-4a1d-9757-ae66904a196a")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.7.1.0")]
+[assembly: AssemblyFileVersion("1.7.1.0")]
