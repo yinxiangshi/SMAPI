f2ac6fbb498d0efa4d397b464010548636034c48
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-10-10 22:48:06 -0400

    bump mod build package version for release

--- src/SMAPI.ModBuildConfig/Properties/AssemblyInfo.cs
+++ src/SMAPI.ModBuildConfig/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: Guid("ea4f1e80-743f-4a1d-9757-ae66904a196a")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.7.1.0")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
