359e1df431fa160993f0640b3458a95a8ee831d7
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-10-29 22:45:13 -0400

    redirect root to log parser (#358)

--- src/SMAPI.Web/Controllers/LogParserController.cs
+++ src/SMAPI.Web/Controllers/LogParserController.cs
@@ -52,6 +52,7 @@ namespace StardewModdingAPI.Web.Controllers
         /// <summary>Render the log parser UI.</summary>
         /// <param name="id">The paste ID.</param>
         [HttpGet]
+        [Route("")]
         [Route("log")]
         [Route("log/{id}")]
         public ViewResult Index(string id = null)
