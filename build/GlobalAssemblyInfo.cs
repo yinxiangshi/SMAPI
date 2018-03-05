1fbd41ecb2141d769072c71822fb02876401a592
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-03-04 21:32:23 -0500

    bump version for release

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("SMAPI")]
-[assembly: AssemblyVersion("2.5.2")]
-[assembly: AssemblyFileVersion("2.5.2")]
+[assembly: AssemblyVersion("2.5.3")]
+[assembly: AssemblyFileVersion("2.5.3")]
