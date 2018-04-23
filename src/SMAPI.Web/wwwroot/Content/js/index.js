82f418a38baeb44f68601708ebbd3c4af03ef6da
Dan Volchek <volchek2@illinois.edu>
2018-04-23 01:58:18 -0500

    add missing semicolon

--- src/SMAPI.Web/wwwroot/Content/js/index.js
+++ src/SMAPI.Web/wwwroot/Content/js/index.js
@@ -5,6 +5,6 @@ $(document).ready(function () {
             pufferchick.attr("src", "Content/images/pufferchick-cool.png");
         }, function () {
             pufferchick.attr("src", "favicon.ico");
-        })
+        });
     });
 });
