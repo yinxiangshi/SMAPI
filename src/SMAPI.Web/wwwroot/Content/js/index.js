371d7fa05317f107f7b97a7b5db93e176039418d
Dan Volchek <volchek2@illinois.edu>
2018-04-23 02:01:14 -0500

    use implicit iteration instead

--- src/SMAPI.Web/wwwroot/Content/js/index.js
+++ src/SMAPI.Web/wwwroot/Content/js/index.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
     var pufferchick = $("#pufferchick");
-    $(".download").each(function (index, element) {
-        $(element).hover(function () {
-            pufferchick.attr("src", "Content/images/pufferchick-cool.png");
-        }, function () {
-            pufferchick.attr("src", "favicon.ico");
-        });
+    $(".download").hover(function () {
+        pufferchick.attr("src", "Content/images/pufferchick-cool.png");
+    }, function () {
+        pufferchick.attr("src", "favicon.ico");
     });
 });
