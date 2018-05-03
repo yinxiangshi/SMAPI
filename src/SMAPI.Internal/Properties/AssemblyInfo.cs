60040854a3b95ab220a42c087bda7f9011dda8ca
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-05-03 01:38:08 -0400

    switch back to shared project due to installer issues

--- src/SMAPI.Internal/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("SMAPI.Internal")]
-[assembly: AssemblyDescription("Contains internal SMAPI code that's shared between its projects.")]
-[assembly: InternalsVisibleTo("StardewModdingAPI")]
-[assembly: InternalsVisibleTo("StardewModdingAPI.ModBuildConfig")]
-[assembly: InternalsVisibleTo("StardewModdingAPI.Installer")]
-[assembly: InternalsVisibleTo("StardewModdingAPI.Web")]
