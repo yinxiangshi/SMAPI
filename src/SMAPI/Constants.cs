5088ecf8c8a04a028b6a9c45f907eae50894ffe6
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-05-03 00:34:36 -0400

    bump beta version

--- src/SMAPI/Constants.cs
+++ src/SMAPI/Constants.cs
@@ -38,10 +38,10 @@ namespace StardewModdingAPI
         ** Public
         ****/
         /// <summary>SMAPI's current semantic version.</summary>
-        public static ISemanticVersion ApiVersion { get; } = new SemanticVersion("2.6-beta.3");
+        public static ISemanticVersion ApiVersion { get; } = new SemanticVersion("2.6-beta.4");
 
         /// <summary>The minimum supported version of Stardew Valley.</summary>
-        public static ISemanticVersion MinimumGameVersion { get; } = new GameVersion("1.3.3");
+        public static ISemanticVersion MinimumGameVersion { get; } = new GameVersion("1.3.5");
 
         /// <summary>The maximum supported version of Stardew Valley.</summary>
         public static ISemanticVersion MaximumGameVersion { get; } = null;
