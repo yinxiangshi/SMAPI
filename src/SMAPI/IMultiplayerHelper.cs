5a2755bfcc516345800a4e9b966a0aff435215b8
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-05-10 00:01:24 -0400

    fix multiplayer.GetActiveLocations not added to interface (#480)

--- src/SMAPI/IMultiplayerHelper.cs
+++ src/SMAPI/IMultiplayerHelper.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using StardewValley;
+
 namespace StardewModdingAPI
 {
     /// <summary>Provides multiplayer utilities.</summary>
@@ -5,5 +8,8 @@ namespace StardewModdingAPI
     {
         /// <summary>Get a new multiplayer ID.</summary>
         long GetNewID();
+
+        /// <summary>Get the locations which are being actively synced from the host.</summary>
+        IEnumerable<GameLocation> GetActiveLocations();
     }
 }
