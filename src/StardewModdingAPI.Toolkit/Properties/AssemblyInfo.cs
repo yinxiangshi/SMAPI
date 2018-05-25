2a7bcb28f6a390e165c2cb8817f333aad750a032
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-05-25 01:13:28 -0400

    add empty toolkit project & fix misleading build configuration name (#532)

---
+++ src/StardewModdingAPI.Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("SMAPI.Toolkit")]
+[assembly: AssemblyDescription("")]
+[assembly: InternalsVisibleTo("StardewModdingAPI")]
