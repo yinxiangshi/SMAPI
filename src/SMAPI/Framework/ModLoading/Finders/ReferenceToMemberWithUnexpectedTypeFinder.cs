a30794894bd9bd3e152c882286f0f3600ea41400
Evan Behar <behare@gmail.com>
2018-07-07 23:46:28 -0700

    Revert ShouldValidate

--- src/SMAPI/Framework/ModLoading/Finders/ReferenceToMemberWithUnexpectedTypeFinder.cs
+++ src/SMAPI/Framework/ModLoading/Finders/ReferenceToMemberWithUnexpectedTypeFinder.cs
@@ -111,18 +111,7 @@ namespace StardewModdingAPI.Framework.ModLoading.Finders
         /// <param name="type">The type reference.</param>
         private bool ShouldValidate(TypeReference type)
         {
-            if (type != null)
-                return true;
-                
-            // Extract scope name from type string representation for compatibility
-            // Under Linux, type.Scope.Name sometimes reports incorrectly
-            string scopeName = type.ToString();
-            if (scopeName[0] != '$')
-                return false;
-
-            scopeName = scopeName.Substring(0, scopeName.IndexOf(".", System.StringComparison.CurrentCulture));
-
-            return this.ValidateReferencesToAssemblies.Contains(scopeName);
+            return type != null && this.ValidateReferencesToAssemblies.Contains(type.Scope.Name);
         }
 
         /// <summary>Get a unique string representation of a type.</summary>
