6f63bd00248fd90649718a1a77926a48f2df0e51
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-04-21 13:23:33 -0400

    update nuspec for alpha release

--- src/SMAPI.ModBuildConfig/Properties/AssemblyInfo.cs
+++ src/SMAPI.ModBuildConfig/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("SMAPI.ModBuildConfig")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
