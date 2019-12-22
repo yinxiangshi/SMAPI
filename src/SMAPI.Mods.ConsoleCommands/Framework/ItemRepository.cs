89de36534d1ce5361af2bb83a1b521e081c1dcf7
Mizzion <36684211+Mizzion@users.noreply.github.com>
2019-12-21 21:36:42 -0700

    Fixed the ring issue and flooring
    Fixed the same issues that CJB Item Spawner had related to the rings and floors.
    
    (Sorry for the delay Christmas preparation is taking up most of my time).

--- src/SMAPI.Mods.ConsoleCommands/Framework/ItemRepository.cs
+++ src/SMAPI.Mods.ConsoleCommands/Framework/ItemRepository.cs
@@ -51,7 +51,7 @@ namespace StardewModdingAPI.Mods.ConsoleCommands.Framework
                     yield return this.TryCreate(ItemType.Wallpaper, id, () => new Wallpaper(id) { Category = SObject.furnitureCategory });
 
                 // flooring
-                for (int id = 0; id < 40; id++)
+                for (int id = 0; id < 56; id++)
                     yield return this.TryCreate(ItemType.Flooring, id, () => new Wallpaper(id, isFloor: true) { Category = SObject.furnitureCategory });
 
                 // equipment
@@ -61,7 +61,7 @@ namespace StardewModdingAPI.Mods.ConsoleCommands.Framework
                     yield return this.TryCreate(ItemType.Hat, id, () => new Hat(id));
                 foreach (int id in Game1.objectInformation.Keys)
                 {
-                    if (id >= Ring.ringLowerIndexRange && id <= Ring.ringUpperIndexRange)
+                    if ((id >= Ring.ringLowerIndexRange && id <= Ring.ringUpperIndexRange) || (id >= 810 && id <= 811))
                         yield return this.TryCreate(ItemType.Ring, id, () => new Ring(id));
                 }
 
@@ -103,7 +103,7 @@ namespace StardewModdingAPI.Mods.ConsoleCommands.Framework
                 {
                     if (id == 79)
                         continue; // secret note handled above
-                    if (id >= Ring.ringLowerIndexRange && id <= Ring.ringUpperIndexRange)
+                    if ((id >= Ring.ringLowerIndexRange && id <= Ring.ringUpperIndexRange) || (id >= 810 && id <= 811))
                         continue; // handled separated
 
                     // spawn main item
