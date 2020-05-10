0b5fa6bf86d08cd2205dca3279d99795e68f5a12
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-05-09 21:30:28 -0400

    add new 'unknown' compatibility status (#711)

--- src/SMAPI.Toolkit/Framework/Clients/Wiki/WikiCompatibilityStatus.cs
+++ src/SMAPI.Toolkit/Framework/Clients/Wiki/WikiCompatibilityStatus.cs
@@ -3,25 +3,28 @@ namespace StardewModdingAPI.Toolkit.Framework.Clients.Wiki
     /// <summary>The compatibility status for a mod.</summary>
     public enum WikiCompatibilityStatus
     {
+        /// <summary>The status is unknown.</summary>
+        Unknown,
+
         /// <summary>The mod is compatible.</summary>
-        Ok = 0,
+        Ok,
 
         /// <summary>The mod is compatible if you use an optional official download.</summary>
-        Optional = 1,
+        Optional,
 
         /// <summary>The mod is compatible if you use an unofficial update.</summary>
-        Unofficial = 2,
+        Unofficial,
 
         /// <summary>The mod isn't compatible, but the player can fix it or there's a good alternative.</summary>
-        Workaround = 3,
+        Workaround,
 
         /// <summary>The mod isn't compatible.</summary>
-        Broken = 4,
+        Broken,
 
         /// <summary>The mod is no longer maintained by the author, and an unofficial update or continuation is unlikely.</summary>
-        Abandoned = 5,
+        Abandoned,
 
         /// <summary>The mod is no longer needed and should be removed.</summary>
-        Obsolete = 6
+        Obsolete
     }
 }
