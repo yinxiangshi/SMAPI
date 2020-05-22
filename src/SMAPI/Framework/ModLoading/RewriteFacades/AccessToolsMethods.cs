4468f390985e4cdff330147b4e6c6089aedfb48c
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-05-21 22:25:09 -0400

    improve facade annotations

--- src/SMAPI/Framework/ModLoading/RewriteFacades/AccessToolsMethods.cs
+++ src/SMAPI/Framework/ModLoading/RewriteFacades/AccessToolsMethods.cs
@@ -8,6 +8,7 @@ namespace StardewModdingAPI.Framework.ModLoading.RewriteFacades
 {
     /// <summary>Maps Harmony 1.x <see cref="AccessTools"/> methods to Harmony 2.x to avoid breaking older mods.</summary>
     /// <remarks>This is public to support SMAPI rewriting and should not be referenced directly by mods.</remarks>
+    [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Used via assembly rewriting")]
     [SuppressMessage("ReSharper", "CS1591", Justification = "Documentation not needed for facade classes.")]
     public class AccessToolsMethods
     {
--- src/SMAPI/Framework/ModLoading/RewriteFacades/HarmonyInstanceMethods.cs
+++ src/SMAPI/Framework/ModLoading/RewriteFacades/HarmonyInstanceMethods.cs
@@ -9,6 +9,7 @@ namespace StardewModdingAPI.Framework.ModLoading.RewriteFacades
 {
     /// <summary>Maps Harmony 1.x <code>HarmonyInstance</code> methods to Harmony 2.x's <see cref="Harmony"/> to avoid breaking older mods.</summary>
     /// <remarks>This is public to support SMAPI rewriting and should not be referenced directly by mods.</remarks>
+    [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Used via assembly rewriting")]
     [SuppressMessage("ReSharper", "CS1591", Justification = "Documentation not needed for facade classes.")]
     public class HarmonyInstanceMethods : Harmony
     {
