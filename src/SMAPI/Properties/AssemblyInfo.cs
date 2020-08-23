cb37644291012a8f4797e009bf0fdcb3ab51845f
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-08-22 21:47:37 -0400

    move assembly attributes to match convention

---
+++ src/SMAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("SMAPI.Tests")]
+[assembly: InternalsVisibleTo("ConsoleCommands")] // for performance monitoring commands
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Moq for unit testing
