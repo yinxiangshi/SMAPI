50a146d1c9a228391c4201685a5e0df9daa529e9
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-12-20 22:34:58 -0500

    update game version

--- docs/release-notes.md
+++ docs/release-notes.md
@@ -7,6 +7,10 @@
   * Migrated to Harmony 2.0 (see [_migrate to Harmony 2.0_](https://stardewvalleywiki.com/Modding:Migrate_to_Harmony_2.0) for more info).
 -->
 
+## Upcoming release
+* For players:
+  * Updated for Stardew Valley 1.5.
+
 ## 3.7.6
 Released 21 November 2020 for Stardew Valley 1.4.1 or later.
 
--- src/SMAPI/Constants.cs
+++ src/SMAPI/Constants.cs
@@ -54,10 +54,10 @@ namespace StardewModdingAPI
         public static ISemanticVersion ApiVersion { get; } = new Toolkit.SemanticVersion("3.7.6");
 
         /// <summary>The minimum supported version of Stardew Valley.</summary>
-        public static ISemanticVersion MinimumGameVersion { get; } = new GameVersion("1.4.1");
+        public static ISemanticVersion MinimumGameVersion { get; } = new GameVersion("1.5.0");
 
         /// <summary>The maximum supported version of Stardew Valley.</summary>
-        public static ISemanticVersion MaximumGameVersion { get; } = new GameVersion("1.4.5");
+        public static ISemanticVersion MaximumGameVersion { get; } = null;
 
         /// <summary>The target game platform.</summary>
         public static GamePlatform TargetPlatform { get; } = EarlyConstants.Platform;
