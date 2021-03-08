b8309e90173172dd88a50b429b760271b4a150cf
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2021-03-08 00:08:15 -0500

    prepare for release

--- docs/release-notes.md
+++ docs/release-notes.md
@@ -7,7 +7,9 @@
   * Migrated to Harmony 2.0 (see [_migrate to Harmony 2.0_](https://stardewvalleywiki.com/Modding:Migrate_to_Harmony_2.0) for more info).
 -->
 
-## Upcoming release
+## 3.9.4
+Released 07 March 2021 for Stardew Valley 1.5.4 or later.
+
 * For players:
   * Fixed installer error if the `Mods` folder doesn't exist in 3.9.3.
 
--- src/SMAPI/Constants.cs
+++ src/SMAPI/Constants.cs
@@ -54,7 +54,7 @@ namespace StardewModdingAPI
         ** Public
         ****/
         /// <summary>SMAPI's current semantic version.</summary>
-        public static ISemanticVersion ApiVersion { get; } = new Toolkit.SemanticVersion("3.9.3");
+        public static ISemanticVersion ApiVersion { get; } = new Toolkit.SemanticVersion("3.9.4");
 
         /// <summary>The minimum supported version of Stardew Valley.</summary>
         public static ISemanticVersion MinimumGameVersion { get; } = new GameVersion("1.5.4");
