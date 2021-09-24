b791e854c1f2117f0cc218485ec767a493a3d847
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2021-09-23 23:11:54 -0400

    fix installer precheck errors showing quotes, tweak readability

--- src/SMAPI.Installer/assets/windows-install.bat
+++ src/SMAPI.Installer/assets/windows-install.bat
@@ -3,7 +3,8 @@
 REM make sure we're not running within a zip folder
 echo "%~dp0" | findstr /C:"%TEMP%" 1>nul
 if %ERRORLEVEL% EQU 0 (
-    echo "Oops! It looks like you're running the installer from inside a zip file. Make sure you unzip the download first."
+    echo Oops! It looks like you're running the installer from inside a zip file. Make sure you unzip the download first.
+    echo.
     pause
     exit
 )
@@ -11,14 +12,17 @@ if %ERRORLEVEL% EQU 0 (
 REM make sure .NET 5 is installed
 WHERE dotnet /q
 if %ERRORLEVEL% NEQ 0 (
-    echo "Oops! You must have .NET 5 (desktop x64) installed to use SMAPI: https://dotnet.microsoft.com/download/dotnet/5.0/runtime"
+    echo Oops! You must have .NET 5 ^(desktop x64^) installed to use SMAPI: https://dotnet.microsoft.com/download/dotnet/5.0/runtime
+    echo.
     pause
     exit
 )
 
 REM make sure an antivirus hasn't deleted the installer DLL
 if not exist internal\windows\SMAPI.Installer.dll (
-    echo "Oops! SMAPI can't find its 'internal\windows\SMAPI.Installer.dll' file. Your antivirus might have deleted the file."
+    echo Oops! SMAPI is missing one of its files. Your antivirus might have deleted it.
+    echo Missing file: %installerDir%internal\windows\SMAPI.Installer.dll
+    echo.
     pause
     exit
 )
