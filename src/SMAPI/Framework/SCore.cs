yinxiangshi/SMAPI

commit f58bcb8fb4eb4a0cbfd1085baf400d922895ca9c

remove last Harmony patches

src/SMAPI/Framework/SCore.cs
@@ -34,8 +34,6 @@ using StardewModdingAPI.Framework.Serialization;
 using StardewModdingAPI.Framework.StateTracking.Snapshots;
 using StardewModdingAPI.Framework.Utilities;
 using StardewModdingAPI.Internal;
-using StardewModdingAPI.Internal.Patching;
-using StardewModdingAPI.Patches;
 using StardewModdingAPI.Toolkit;
 using StardewModdingAPI.Toolkit.Framework.Clients.WebApi;
 using StardewModdingAPI.Toolkit.Framework.ModData;
@@ -273,19 +271,17 @@ namespace StardewModdingAPI.Framework
                     exitGameImmediately: this.ExitGameImmediately,
 
                     onGameContentLoaded: this.OnInstanceContentLoaded,
+                    onLoadStageChanged: this.OnLoadStageChanged,
                     onGameUpdating: this.OnGameUpdating,
                     onPlayerInstanceUpdating: this.OnPlayerInstanceUpdating,
                     onPlayerInstanceRendered: this.OnRendered,
                     onGameExiting: this.OnGameExiting
                 );
-                StardewValley.GameRunner.instance = this.Game;
+                GameRunner.instance = this.Game;
+                TitleMenu.OnCreatedNewCharacter += () => this.OnLoadStageChanged(LoadStage.CreatedBasicInfo);
 
-                // apply game patches
+                // fix Harmony for mods
                 MiniMonoModHotfix.Apply();
-                HarmonyPatcher.Apply("SMAPI", this.Monitor,
-                    new Game1Patcher(this.OnLoadStageChanged),
-                    new TitleMenuPatcher(this.OnLoadStageChanged)
-                );
 
                 // set window titles
                 this.UpdateWindowTitles();

src/SMAPI/Framework/SGame.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI.Enums;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Framework.Input;
 using StardewModdingAPI.Framework.Reflection;
@@ -12,6 +13,8 @@ using StardewModdingAPI.Internal;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.Logging;
+using StardewValley.Menus;
+using StardewValley.Minigames;
 
 namespace StardewModdingAPI.Framework
 {
@@ -44,6 +47,9 @@ namespace StardewModdingAPI.Framework
 
         /// <summary>Raised after the instance finishes loading its initial content.</summary>
         private readonly Action OnContentLoaded;
+
+        /// <summary>Raised invoke when the load stage changes through a method like <see cref="Game1.CleanupReturningToTitle"/>.</summary>
+        private readonly Action<LoadStage> OnLoadStageChanged;
 
         /// <summary>Raised after the instance finishes a draw loop.</summary>
         private readonly Action<RenderTarget2D> OnRendered;
@@ -98,8 +104,9 @@ namespace StardewModdingAPI.Framework
         /// <param name="exitGameImmediately">Immediately exit the game without saving. This should only be invoked when an irrecoverable fatal error happens that risks save corruption or game-breaking bugs.</param>
         /// <param name="onUpdating">Raised when the instance is updating its state (roughly 60 times per second).</param>
         /// <param name="onContentLoaded">Raised after the game finishes loading its initial content.</param>
+        /// <param name="onLoadStageChanged">Raised invoke when the load stage changes through a method like <see cref="Game1.CleanupReturningToTitle"/>.</param>
         /// <param name="onRendered">Raised after the instance finishes a draw loop.</param>
-        public SGame(PlayerIndex playerIndex, int instanceIndex, Monitor monitor, Reflector reflection, SInputState input, SModHooks modHooks, IGameLogger gameLogger, SMultiplayer multiplayer, Action<string> exitGameImmediately, Action<SGame, GameTime, Action> onUpdating, Action onContentLoaded, Action<RenderTarget2D> onRendered)
+        public SGame(PlayerIndex playerIndex, int instanceIndex, Monitor monitor, Reflector reflection, SInputState input, SModHooks modHooks, IGameLogger gameLogger, SMultiplayer multiplayer, Action<string> exitGameImmediately, Action<SGame, GameTime, Action> onUpdating, Action onContentLoaded, Action<LoadStage> onLoadStageChanged, Action<RenderTarget2D> onRendered)
             : base(playerIndex, instanceIndex)
         {
             // init XNA
@@ -118,6 +125,7 @@ namespace StardewModdingAPI.Framework
             this.ExitGameImmediately = exitGameImmediately;
             this.OnUpdating = onUpdating;
             this.OnContentLoaded = onContentLoaded;
+            this.OnLoadStageChanged = onLoadStageChanged;
             this.OnRendered = onRendered;
         }
 
@@ -166,6 +174,20 @@ namespace StardewModdingAPI.Framework
             // The Initial* fields should no longer be used after this point, since mods may further override them after initialization.
             this.InitialInput = null;
             this.InitialMultiplayer = null;
+        }
+
+        /// <summary>The method called when loading or creating a save.</summary>
+        /// <param name="loadedGame">Whether this is being called from the game's load enumerator.</param>
+        public override void loadForNewGame(bool loadedGame = false)
+        {
+            base.loadForNewGame(loadedGame);
+
+            bool isCreating =
+                (Game1.currentMinigame is Intro) // creating save with intro
+                || (Game1.activeClickableMenu is TitleMenu menu && menu.transitioningCharacterCreationMenu); // creating save, skipped intro
+
+            if (isCreating)
+                this.OnLoadStageChanged(LoadStage.CreatedLocations);
         }
 
         /// <summary>The method called when the instance is updating its state (roughly 60 times per second).</summary>
@@ -235,6 +257,14 @@ namespace StardewModdingAPI.Framework
                 }
             }
             Context.IsInDrawLoop = false;
+        }
+
+        /// <summary>The method called when the game is returning to the title screen from a loaded save.</summary>
+        public override void CleanupReturningToTitle()
+        {
+            this.OnLoadStageChanged(LoadStage.ReturningToTitle);
+
+            base.CleanupReturningToTitle();
         }
     }
 }

src/SMAPI.Internal.Patching/BasePatcher.cs
@@ -1,54 +1,0 @@
-using System;
-using System.Reflection;
-using HarmonyLib;
-
-namespace StardewModdingAPI.Internal.Patching
-{
-    /// <summary>Provides base implementation logic for <see cref="IPatcher"/> instances.</summary>
-    internal abstract class BasePatcher : IPatcher
-    {
-        /*********
-        ** Public methods
-        *********/
-        /// <inheritdoc />
-        public abstract void Apply(Harmony harmony, IMonitor monitor);
-
-
-        /*********
-        ** Protected methods
-        *********/
-        /// <summary>Get a method and assert that it was found.</summary>
-        /// <typeparam name="TTarget">The type containing the method.</typeparam>
-        /// <param name="parameters">The method parameter types, or <c>null</c> if it's not overloaded.</param>
-        protected ConstructorInfo RequireConstructor<TTarget>(params Type[] parameters)
-        {
-            return PatchHelper.RequireConstructor<TTarget>(parameters);
-        }
-
-        /// <summary>Get a method and assert that it was found.</summary>
-        /// <typeparam name="TTarget">The type containing the method.</typeparam>
-        /// <param name="name">The method name.</param>
-        /// <param name="parameters">The method parameter types, or <c>null</c> if it's not overloaded.</param>
-        /// <param name="generics">The method generic types, or <c>null</c> if it's not generic.</param>
-        protected MethodInfo RequireMethod<TTarget>(string name, Type[]? parameters = null, Type[]? generics = null)
-        {
-            return PatchHelper.RequireMethod<TTarget>(name, parameters, generics);
-        }
-
-        /// <summary>Get a Harmony patch method on the current patcher instance.</summary>
-        /// <param name="name">The method name.</param>
-        /// <param name="priority">The patch priority to apply, usually specified using Harmony's <see cref="Priority"/> enum, or <c>null</c> to keep the default value.</param>
-        protected HarmonyMethod GetHarmonyMethod(string name, int? priority = null)
-        {
-            HarmonyMethod method = new(
-                AccessTools.Method(this.GetType(), name)
-                ?? throw new InvalidOperationException($"Can't find patcher method {PatchHelper.GetMethodString(this.GetType(), name)}.")
-            );
-
-            if (priority.HasValue)
-                method.priority = priority.Value;
-
-            return method;
-        }
-    }
-}

src/SMAPI.Internal.Patching/HarmonyPatcher.cs
@@ -1,36 +1,0 @@
-using System;
-using HarmonyLib;
-
-namespace StardewModdingAPI.Internal.Patching
-{
-    /// <summary>Simplifies applying <see cref="IPatcher"/> instances to the game.</summary>
-    internal static class HarmonyPatcher
-    {
-        /*********
-        ** Public methods
-        *********/
-        /// <summary>Apply the given Harmony patchers.</summary>
-        /// <param name="id">The mod ID applying the patchers.</param>
-        /// <param name="monitor">The monitor with which to log any errors.</param>
-        /// <param name="patchers">The patchers to apply.</param>
-        public static Harmony Apply(string id, IMonitor monitor, params IPatcher[] patchers)
-        {
-            Harmony harmony = new(id);
-
-            foreach (IPatcher patcher in patchers)
-            {
-                try
-                {
-                    patcher.Apply(harmony, monitor);
-                }
-                catch (Exception ex)
-                {
-                    monitor.Log($"Couldn't apply runtime patch '{patcher.GetType().Name}' to the game. Some SMAPI features may not work correctly. See log file for details.", LogLevel.Error);
-                    monitor.Log($"Technical details:\n{ex.GetLogSummary()}");
-                }
-            }
-
-            return harmony;
-        }
-    }
-}

src/SMAPI.Internal.Patching/IPatcher.cs
@@ -1,16 +1,0 @@
-using HarmonyLib;
-
-namespace StardewModdingAPI.Internal.Patching
-{
-    /// <summary>A set of Harmony patches to apply.</summary>
-    internal interface IPatcher
-    {
-        /*********
-        ** Public methods
-        *********/
-        /// <summary>Apply the Harmony patches for this instance.</summary>
-        /// <param name="harmony">The Harmony instance.</param>
-        /// <param name="monitor">The monitor with which to log any errors.</param>
-        public void Apply(Harmony harmony, IMonitor monitor);
-    }
-}

src/SMAPI.Internal.Patching/PatchHelper.cs
@@ -1,77 +1,0 @@
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using HarmonyLib;
-
-namespace StardewModdingAPI.Internal.Patching
-{
-    /// <summary>Provides utility methods for patching game code with Harmony.</summary>
-    internal static class PatchHelper
-    {
-        /*********
-        ** Public methods
-        *********/
-        /// <summary>Get a constructor and assert that it was found.</summary>
-        /// <typeparam name="TTarget">The type containing the method.</typeparam>
-        /// <param name="parameters">The method parameter types, or <c>null</c> if it's not overloaded.</param>
-        /// <exception cref="InvalidOperationException">The type has no matching constructor.</exception>
-        public static ConstructorInfo RequireConstructor<TTarget>(Type[]? parameters = null)
-        {
-            return
-                AccessTools.Constructor(typeof(TTarget), parameters)
-                ?? throw new InvalidOperationException($"Can't find constructor {PatchHelper.GetMethodString(typeof(TTarget), null, parameters)} to patch.");
-        }
-
-        /// <summary>Get a method and assert that it was found.</summary>
-        /// <typeparam name="TTarget">The type containing the method.</typeparam>
-        /// <param name="name">The method name.</param>
-        /// <param name="parameters">The method parameter types, or <c>null</c> if it's not overloaded.</param>
-        /// <param name="generics">The method generic types, or <c>null</c> if it's not generic.</param>
-        /// <exception cref="InvalidOperationException">The type has no matching method.</exception>
-        public static MethodInfo RequireMethod<TTarget>(string name, Type[]? parameters = null, Type[]? generics = null)
-        {
-            return
-                AccessTools.Method(typeof(TTarget), name, parameters, generics)
-                ?? throw new InvalidOperationException($"Can't find method {PatchHelper.GetMethodString(typeof(TTarget), name, parameters, generics)} to patch.");
-        }
-
-        /// <summary>Get a human-readable representation of a method target.</summary>
-        /// <param name="type">The type containing the method.</param>
-        /// <param name="name">The method name, or <c>null</c> for a constructor.</param>
-        /// <param name="parameters">The method parameter types, or <c>null</c> if it's not overloaded.</param>
-        /// <param name="generics">The method generic types, or <c>null</c> if it's not generic.</param>
-        public static string GetMethodString(Type type, string? name, Type[]? parameters = null, Type[]? generics = null)
-        {
-            StringBuilder str = new();
-
-            // type
-            str.Append(type.FullName);
-
-            // method name (if not constructor)
-            if (name != null)
-            {
-                str.Append('.');
-                str.Append(name);
-            }
-
-            // generics
-            if (generics?.Any() == true)
-            {
-                str.Append('<');
-                str.Append(string.Join(", ", generics.Select(p => p.FullName)));
-                str.Append('>');
-            }
-
-            // parameters
-            if (parameters?.Any() == true)
-            {
-                str.Append('(');
-                str.Append(string.Join(", ", parameters.Select(p => p.FullName)));
-                str.Append(')');
-            }
-
-            return str.ToString();
-        }
-    }
-}

src/SMAPI.Internal.Patching/SMAPI.Internal.Patching.projitems
@@ -1,17 +1,0 @@
-﻿<?xml version="1.0" encoding="utf-8"?>
-<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <PropertyGroup>
-    <MSBuildAllProjects Condition="'$(MSBuildVersion)' == '' Or '$(MSBuildVersion)' &lt; '16.0'">$(MSBuildAllProjects);$(MSBuildThisFileFullPath)</MSBuildAllProjects>
-    <HasSharedItems>true</HasSharedItems>
-    <SharedGUID>6c16e948-3e5c-47a7-bf4b-07a7469a87a5</SharedGUID>
-  </PropertyGroup>
-  <PropertyGroup Label="Configuration">
-    <Import_RootNamespace>SMAPI.Internal.Patching</Import_RootNamespace>
-  </PropertyGroup>
-  <ItemGroup>
-    <Compile Include="$(MSBuildThisFileDirectory)BasePatcher.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)HarmonyPatcher.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)IPatcher.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)PatchHelper.cs" />
-  </ItemGroup>
-</Project>

src/SMAPI.Internal.Patching/SMAPI.Internal.Patching.shproj
@@ -1,13 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<Project ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <PropertyGroup Label="Globals">
-    <ProjectGuid>6c16e948-3e5c-47a7-bf4b-07a7469a87a5</ProjectGuid>
-    <MinimumVisualStudioVersion>14.0</MinimumVisualStudioVersion>
-  </PropertyGroup>
-  <Import Project="$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props" Condition="Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')" />
-  <Import Project="$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\CodeSharing\Microsoft.CodeSharing.Common.Default.props" />
-  <Import Project="$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\CodeSharing\Microsoft.CodeSharing.Common.props" />
-  <PropertyGroup />
-  <Import Project="SMAPI.Internal.Patching.projitems" Label="Shared" />
-  <Import Project="$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\CodeSharing\Microsoft.CodeSharing.CSharp.targets" />
-</Project>

commit 507650353859876d47ecc46402bd6f169a0c0203

fix load stage no longer updated on initial save creation
This broke in f58bcb8fb4eb4a0cbfd1085baf400d922895ca9c because Game1.Initialize() resets static fields, so we need to register `TitleMenu.OnCreatedNewCharacter` after Initialize() runs.

src/SMAPI/Framework/SCore.cs
@@ -278,7 +278,6 @@ namespace StardewModdingAPI.Framework
                     onGameExiting: this.OnGameExiting
                 );
                 GameRunner.instance = this.Game;
-                TitleMenu.OnCreatedNewCharacter += () => this.OnLoadStageChanged(LoadStage.CreatedBasicInfo);
 
                 // fix Harmony for mods
                 MiniMonoModHotfix.Apply();

src/SMAPI/Framework/SGame.cs
@@ -170,6 +170,8 @@ namespace StardewModdingAPI.Framework
             // The game resets public static fields after the class is constructed (see GameRunner.SetInstanceDefaults), so SMAPI needs to re-override them here.
             Game1.input = this.InitialInput;
             Game1.multiplayer = this.InitialMultiplayer;
+            if (this.IsMainInstance)
+                TitleMenu.OnCreatedNewCharacter += () => this.OnLoadStageChanged(LoadStage.CreatedBasicInfo); // event is static and shared between screens
 
             // The Initial* fields should no longer be used after this point, since mods may further override them after initialization.
             this.InitialInput = null;